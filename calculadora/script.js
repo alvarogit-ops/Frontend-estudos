2659ec0bb4d2ed74a7d1f08ae7d5ee83d61b2b3a
Alvaro Yuri <alvaro.yuri@outlook.com>
2026-09-18 17:17:37 -0300

    Implementando calculadora

--- calculadora/script.js
+++ calculadora/script.js
@@ -12,3 +12,11 @@ function calc(){
 function backspace(){
     resultado.value = resultado.value.substring(0, resultado.value.length - 1)
 }
+
+function clean(){
+    resultado.value = ''
+}
+
+if (Number.isFinite(Infinity)){
+    resultado.value = 'Erro'
+}
